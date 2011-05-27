8e3cf3f321ad1d296e751b52cd226596a3923960
Dustin Sallings <dustin@spy.net>
2011-05-26 23:49:18 -0700

    B&W status menu icons.
    Change-Id: I428a15390ade534f23a1fa4a5b4096500a49b29c
    Reviewed-on: http://review.membase.org/6587
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- Membase/MembaseAppDelegate.m
+++ Membase/MembaseAppDelegate.m
@@ -35,7 +35,7 @@
 
 -(void)teardown:(id)sender {
     shuttingDown = YES;
-    NSImage *statusIcon = [NSImage imageNamed:@"Membase-status-shutdown.png"];
+    NSImage *statusIcon = [NSImage imageNamed:@"Membase-status-shutdown-bw.png"];
     [statusBar setImage: statusIcon];
     [self stopTask];
 }
@@ -63,7 +63,7 @@
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     
     statusBar=[[NSStatusBar systemStatusBar] statusItemWithLength: 30.0];
-    NSImage *statusIcon = [NSImage imageNamed:@"Membase-status.png"];
+    NSImage *statusIcon = [NSImage imageNamed:@"Membase-status-bw.png"];
     [statusBar setImage: statusIcon];
     [statusBar setMenu: statusMenu];
     [statusBar setEnabled:YES];
